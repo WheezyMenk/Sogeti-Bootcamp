147f46254e9a17f7654a32f2e768ab694f221fe6
Menk <Mary.Menk@us.sogeti.com>
2020-01-29 14:25:28 -0500

    connect of db,add bootstrap, display in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+ constructor(props){
+   super(props)
+   this.state={
+     employees:[],
+   }
+  }
+  componentDidMount(){ // when component "init" basically, do this
+    fetch('http://localhost:3005/SogetiNewHires')//call to API
+    .then(employees => employees.json()) //assign data from call to employees
+    .then(employees_data => { 
+      console.log(employees_data) //log it for funsies
+      this.setState({
+        employees: employees_data,  //update the state w/ the data
+      })
+    }) 
+    .catch(err =>{ console.log(err)}) //catch any errors
+    
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1> Sogeit New Hire Directory </h1>
+          <div className = "row">
+            {this.state.employees.map((employee, index) =>{ //map employee by index
+            return(
+              <div className="media col-12  mb-4 p-3 directory-entry">
+                <img className="mr-3 directory-image" src = {employee.imageURL}  alt="..."/>
+                <div className="media-body">
+                <h5 className="mt-0" key = {index}> {employee.name}</h5> 
+                  <div className="media col-12">
+                  
+                    <h6 className="mt-0 col6 ml-1" key = {index}> {employee.name} is part of Sogeti's January 2020 class. 
+                    {employee.gender==="M" ? "He":"She"} is working as a {employee.position} in our {employee.practice} practice. 
+                    {employee.name} located in {employee.location}. </h6>
+                  
+                    <ul className="list-group col-6 ml-1 list-group-flush contact-info" > 
+                      Contact Information
+                      <li className="list-group-item " key = {index}>{employee.email}</li>
+                      <li className="list-group-item " key = {index}>{employee.phone}</li>
+                      <li className="list-group-item " key = {index}>{employee.gitHub}</li>
+                    
+                    </ul>
+                  </div>
+                                  
+                </div>
+              </div>
+              )
+            }
+            )}
+          </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
